76134629eaf6c5d2b1fb85960bebf1d8e0aa432f
winTS <huynhts@gmail.com>
2019-08-15 23:28:50 -0700

    added colors to log

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,10 @@
 var mysql = require("mysql");
 var inquirer = require ("inquirer");
+var colors = require('colors/safe');
 
 var buyID = '';
 var buyQuantity = 0;
+var stockCheck = 0;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -23,33 +25,35 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
 
-
-
-    connection.query('SELECT * FROM bamazon.products', function(err, response){
-        if(err) throw err;
-
-        console.table(response);
-        bamazonStart();
-    });
+    showDatabase();
 
 });
 
+function showDatabase () {
+  connection.query('SELECT * FROM bamazon.products', function(err, response){
+    if(err) throw err;
+    
+    console.table(response);
+
+    bamazonStart();
+  });
+}
+
 function bamazonStart () {
-    inquirer
-    .prompt({
+  inquirer.prompt({
       name: "userStart",
       type: "list",
       message: "Would you like to buy an item from Bamazon?? [YES] or [NO]",
       choices: ["YES", "NO"]
-    })
-    .then(function(answer) {
+  })
+  .then(function(answer) {
       // based on their answer, either call the bid or the post functions
       if (answer.userStart === "YES") {
         buyItem();
       } else{
         connection.end();
       }
-    });
+  });
 };
 
 function buyItem () {
@@ -78,10 +82,6 @@ function purchaseQuantity () {
     .then(function(answer) {
       // based on their answer, either call the bid or the post functions
       buyQuantity = answer.itemQuantity;
-
-      //set up if statement to compare buyQuantity to available quantity
-      //set up mysql connection to take in price and multiply by buyQuantity for total purchase amount
-      //update mysql table by taking stock quantity and subtract buyQuantity
   
       checkStock();
     });
@@ -93,14 +93,14 @@ function checkStock () {
   connection.query("SELECT stock_quantity FROM products WHERE item_id=?", buyID, function(err, response) {
     if(err) throw err;
 
-    var stockCheck = +response[0].stock_quantity - +buyQuantity;
-
-    console.log(stockCheck);
+    stockCheck = +response[0].stock_quantity - +buyQuantity;
 
     if (stockCheck > 0) {
       ringUp ();
     } else {
-      console.log(`We're sorry, but we are out of item ${buyID}`);
+      console.log(colors.yellow.bgBlack(`We're sorry, but either we are out of item ${buyID} or we do not have enough stock (${+response[0].stock_quantity} available). \nPlease adjust the quantity you'd like to order or check back in a few weeks for a restock. Thank you!`));
+
+      showDatabase();
     }
 
   });
@@ -110,6 +110,23 @@ function ringUp () {
   connection.query("SELECT price FROM products WHERE item_id=?", buyID, function(err, response) {
     if(err) throw err;
 
-    console.log(+response[0].price * +buyQuantity);
+    var orderTotal = (+response[0].price * +buyQuantity).toFixed(2);
+
+    console.log(colors.red.bold(`Your order total will be $${orderTotal}.`));
+
+    updateStock();
+  });
+};
+
+function updateStock () {
+  var sqlUpdate = `UPDATE products SET stock_quantity=? WHERE item_id=?`;
+  var item_data= [stockCheck, buyID];
+
+  connection.query(sqlUpdate, item_data, function(err, response) {
+    if(err) throw err;
+
+  console.log(colors.red.bold(`You are now purchasing ${buyQuantity} of item ${buyID}. There are now ${stockCheck} available for purchase. \nThank you for your business. Have a great day!`));
+
+  showDatabase();
   });
 };
